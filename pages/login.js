a7ab25da5a39260a434afd87aa17a1e7012ed890
med azaza <hamaaz666@gmail.com>
2021-08-31 16:41:12 +0100

    login page design

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,71 @@
 import styles from "../styles/Login.module.scss";
 import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { TextField, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { motion } from "framer-motion";
 import MainHeader from "../components/mainHeader";
 
+const useStyles = makeStyles({
+  root: {
+    width: 350,
+    marginBottom: 20,
+    "& .MuiInput-underline:before": {
+      borderColor: "white",
+    },
+    "&:hover .MuiInput-underline:before": {
+      borderColor: "white",
+    },
+    "& .MuiFormLabel-root": {
+      color: "white",
+      opacity: 0.5,
+    },
+    "& .MuiInputBase-input": {
+      color: "white",
+    },
+  },
+});
+
 export default function Login() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [isValid, setIsValid] = useState({ email: true, pass: true });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert(`${email}, ${pass}`);
+  };
+  useEffect(() => {
+    if (email.length < 6 && email.length > 0) {
+      setIsValid((old) => {
+        let obj = { ...old };
+        obj.email = false;
+        return obj;
+      });
+    } else {
+      setIsValid((old) => {
+        let obj = { ...old };
+        obj.email = true;
+        return obj;
+      });
+    }
+  }, [email]);
+  useEffect(() => {
+    if (pass.length < 6 && pass.length > 0) {
+      setIsValid((old) => {
+        let obj = { ...old };
+        obj.pass = false;
+        return obj;
+      });
+    } else {
+      setIsValid((old) => {
+        let obj = { ...old };
+        obj.pass = true;
+        return obj;
+      });
+    }
+  }, [pass]);
   return (
     <motion.div
       initial={{ opacity: 0, x: 1000 }}
@@ -12,9 +74,27 @@ export default function Login() {
       className={styles.container}
     >
       <MainHeader title="TnFeed - login" />
-      login page
-      <Link href="/">go back</Link>
-      <Link href="/register">go reg</Link>
+      <Image src="/logo.svg" height={100} width={250} />
+      <form onSubmit={submitHandler}>
+        <TextField
+          required
+          error={isValid.email ? false : true}
+          className={classes.root}
+          label="Email"
+          helperText={`${!isValid.email ? "invalid email" : ""}`}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          required
+          error={isValid.pass ? false : true}
+          className={classes.root}
+          label="Password"
+          type="password"
+          helperText={`${!isValid.pass ? "invalid password" : ""}`}
+          onChange={(e) => setPass(e.target.value.toString())}
+        />
+        <Button type="submit">Login</Button>
+      </form>
     </motion.div>
   );
 }
