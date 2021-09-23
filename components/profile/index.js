ca265a75bb81cd2fc202a8ca9c5f3469323eefe2
med_azaza <hamaaz666@gmail.com>
2021-09-23 11:45:24 +0100

    update profile and cover pics

--- components/profile/index.js
+++ components/profile/index.js
@@ -2,31 +2,178 @@ import { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useRouter } from "next/router";
 import styles from "./Profile.module.scss";
+import { styled } from "@mui/material/styles";
+import LoadingButton from "@mui/lab/LoadingButton";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+} from "@mui/material";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 export default function Profile({ token, userData }) {
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  const [profileDial, setProfileDial] = useState(false);
+  const [coverDial, setCoverDial] = useState(false);
+  const [file, setFile] = useState(null);
+  const [uploadLoading, setUploadLoading] = useState(false);
 
   const router = useRouter();
 
+  const updateCoverPic = () => {
+    setUploadLoading(true);
+    let body = {};
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "preset");
+    data.append("cloud_name", "medazcloud");
+    fetch(process.env.NEXT_PUBLIC_UPLOAD, {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        body.coverPicture = data.secure_url;
+        fetch(`${process.env.NEXT_PUBLIC_BASE_API}user/update`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(body),
+        })
+          .then((response) => {
+            router.reload();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+  };
+
+  const updateProfilePic = () => {
+    setUploadLoading(true);
+    let body = {};
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "preset");
+    data.append("cloud_name", "medazcloud");
+    fetch(process.env.NEXT_PUBLIC_UPLOAD, {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        body.profilePicture = data.secure_url;
+        fetch(`${process.env.NEXT_PUBLIC_BASE_API}user/update`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(body),
+        })
+          .then((response) => {
+            router.reload();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      });
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.coverPic}>
+      <div className={styles.coverPic} onClick={() => setCoverDial(true)}>
         {userData.coverPicture && (
-          <img
-            src={`${process.env.NEXT_PUBLIC_BASE_API}images/${userData.coverPicture}`}
-            alt=""
-          />
+          <img src={`${userData.coverPicture}`} alt="" />
         )}
       </div>
-      <div className={styles.profilePic}>
+      <div className={styles.profilePic} onClick={() => setProfileDial(true)}>
         {userData.profilePicture && (
-          <img
-            src={`${process.env.NEXT_PUBLIC_BASE_API}images/${userData.profilePicture}`}
-            alt=""
-          />
+          <img src={`${userData.profilePicture}`} alt="" />
         )}
       </div>
       <p className={styles.name}>{userData.name}</p>
+      <Dialog open={profileDial} onClose={() => setProfileDial(false)}>
+        <DialogTitle>Update Profile Picture</DialogTitle>
+        <DialogContent>
+          <label htmlFor="profilePic">
+            <Input
+              accept="image/*"
+              id="profilePic"
+              type="file"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <Button component="span">upload</Button>
+          </label>
+          <div className={styles.profileImgPre}>
+            {file && (
+              <>
+                <img src={URL.createObjectURL(file)} />
+              </>
+            )}
+          </div>
+          <Button disabled={Boolean(!file)} onClick={() => setFile(null)}>
+            cancel
+          </Button>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setProfileDial(false);
+              setFile(null);
+            }}
+          >
+            Cancel
+          </Button>
+          <LoadingButton onClick={updateProfilePic} loading={uploadLoading}>
+            update
+          </LoadingButton>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={coverDial} onClose={() => setCoverDial(false)}>
+        <DialogTitle>Update Cover Picture</DialogTitle>
+        <DialogContent>
+          <label htmlFor="coverPic">
+            <Input
+              accept="image/*"
+              id="coverPic"
+              type="file"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <Button component="span">upload</Button>
+          </label>
+          <div className={styles.coverImgPre}>
+            {file && (
+              <>
+                <img src={URL.createObjectURL(file)} />
+              </>
+            )}
+          </div>
+          <Button disabled={Boolean(!file)} onClick={() => setFile(null)}>
+            cancel
+          </Button>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setCoverDial(false);
+              setFile(null);
+            }}
+          >
+            Cancel
+          </Button>
+          <LoadingButton onClick={updateCoverPic} loading={uploadLoading}>
+            update
+          </LoadingButton>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
