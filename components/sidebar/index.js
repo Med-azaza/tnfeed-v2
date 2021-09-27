a1d468e6391516ee2bc2f00e0f50c63482d84afb
med_azaza <hamaaz666@gmail.com>
2021-09-27 18:32:09 +0100

    sidebar first section style

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,5 +1,37 @@
 import styles from "./Sidebar.module.scss";
+import { Button, Avatar } from "@mui/material";
 
 export default function Sidebar() {
-  return <div>sidebar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <p>REQUESTS</p>
+        <span>2</span>
+      </div>
+      <div className={styles.request}>
+        <div>
+          <Avatar variant="rounded">A</Avatar>
+          <p>
+            <span>userName</span> wants to add you to friends
+          </p>
+        </div>
+        <div>
+          <Button>accept</Button>
+          <Button>decline</Button>
+        </div>
+      </div>
+      <div className={styles.request}>
+        <div>
+          <Avatar variant="rounded">A</Avatar>
+          <p>
+            <span>userName</span> wants to add you to friends
+          </p>
+        </div>
+        <div>
+          <Button>accept</Button>
+          <Button>decline</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
