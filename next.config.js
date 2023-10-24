69b6b56d90b48db1e5cbb0d42637bbe5b6416b21
Med-azaza <76793308+Med-azaza@users.noreply.github.com>
2023-10-24 16:40:16 +0100

    update images domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ["tnfeed.herokuapp.com"],
+    domains: ["tnfeed-backend.onrender.com"],
   },
 };
